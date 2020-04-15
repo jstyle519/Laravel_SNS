a383d64c064f7e0ef8f0996efac7f9c6a89fc07c
”伊東淳” <j.style.0519.i@gmail.com>
2020-04-15 13:37:01 +0900

    #タグ自動

--- app/Tag.php
+++ app/Tag.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
@@ -10,4 +11,9 @@ class Tag extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function getHashtagAttribute(): string
+    {
+        return '#' . $this->name;
+    }
 }
